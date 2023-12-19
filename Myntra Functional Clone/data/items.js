66ff6ff878430c4803749557ed8aced3e1bcf923
Gourav Kumar Dhiman <115342309+Gourav224@users.noreply.github.com>
2023-12-19 16:42:48 +0530

    resolve image error

--- Myntra Functional Clone/data/items.js
+++ Myntra Functional Clone/data/items.js
@@ -1,7 +1,7 @@
 const items = [
     {
         id: '001',
-        image: '/images/1.jpg',
+        image: 'images/1.jpg',
         company: 'Carlton London',
         item_name: 'Rhodium-Plated CZ Floral Studs',
         original_price: 1045,
@@ -16,7 +16,7 @@ const items = [
     },
     {
         id: '002',
-        image: '/images/2.jpg',
+        image: 'images/2.jpg',
         company: 'CUKOO',
         item_name: 'Women Padded Halter Neck Swimming Dress',
         original_price: 2599,
@@ -31,7 +31,7 @@ const items = [
     },
     {
         id: '003',
-        image: '/images/3.jpg',
+        image: 'images/3.jpg',
         company: 'NUEVOSDAMAS',
         item_name: 'Women Red & White Printed A-Line Knee-Length Skirts',
         original_price: 1599,
@@ -46,7 +46,7 @@ const items = [
     },
     {
         id: '004',
-        image: '/images/4.jpg',
+        image: 'images/4.jpg',
         company: 'ADIDAS',
         item_name: 'Indian Cricket ODI Jersey',
         original_price: 999,
@@ -61,7 +61,7 @@ const items = [
     },
     {
         id: '005',
-        image: '/images/5.jpg',
+        image: 'images/5.jpg',
         company: 'Roadster',
         item_name: 'Pure Cotton T-shirt',
         original_price: 1399,
@@ -76,7 +76,7 @@ const items = [
     },
     {
         id: '006',
-        image: '/images/6.jpg',
+        image: 'images/6.jpg',
         company: 'Nike',
         item_name: 'Men ReactX Running Shoes',
         original_price: 14995,
@@ -91,7 +91,7 @@ const items = [
     },
     {
         id: '007',
-        image: '/images/7.jpg',
+        image: 'images/7.jpg',
         company: 'The Indian Garage Co',
         item_name: 'Men Slim Fit Regular Shorts',
         original_price: 1599,
@@ -106,7 +106,7 @@ const items = [
     },
     {
         id: '008',
-        image: '/images/8.jpg',
+        image: 'images/8.jpg',
         company: 'Nivea',
         item_name: 'Men Fresh Deodrant 150ml',
         original_price: 285,
